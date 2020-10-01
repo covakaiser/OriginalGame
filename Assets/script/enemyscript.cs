9e4f3b7c74f640b6719ba4bdd899fa60c3458a6d
first commit <>
2020-10-02 01:00:56 +0900

    音げっぽいものを一旦作りました

---
+++ Assets/script/enemyscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyscript : MonoBehaviour
+{
+    float speed = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/script/playerscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour
+{
+    float speed = 10.0f;
+    float jump = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity += new Vector3(0, jump, 0);
+        }
+       
+    }
+}
