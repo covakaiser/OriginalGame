3040b01adb6cea6a5386310167a77b13b213e723
first commit <>
2020-10-04 16:30:30 +0900

    unitychanの追加、背景の追加、背景の移動、unitychangが走るアニメーションを追加しました。

---
+++ Assets/script/Wallscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallscript : MonoBehaviour
+{
+    public GameObject wall;
+    float timer = 0;
+    float interval = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            Instantiate(wall, new Vector3(30f,0,30), new Quaternion(0,0,180,0));
+            timer = 0;
+        }
+    }
+}
